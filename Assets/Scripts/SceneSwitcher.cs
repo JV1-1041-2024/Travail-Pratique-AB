6242d56491b0f6cfa74b2bde9cbf84fb146e490a
Walbio <AlexandreSoftware@proton.me>
2024-10-25 08:20:19 -0400

    Changement script change scene, global

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -3,17 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // This method loads the Chambre scene
-    public void LoadChambreScene()
+    // This method loads a specified scene
+    public void LoadScene(string sceneName)
     {
         // Check if the scene is available in the build settings
-        if (Application.CanStreamedLevelBeLoaded("Chambre"))
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
         {
-            SceneManager.LoadScene("Chambre");
+            SceneManager.LoadScene(sceneName);
         }
         else
         {
-            Debug.LogError("Scene 'Chambre' could not be found in build settings!");
+            Debug.LogError($"Scene '{sceneName}' could not be found in build settings!");
         }
+    }
+
+    // Method to load the Chambre scene
+    public void LoadChambreScene()
+    {
+        LoadScene("Chambre");
+    }
+
+    // Method to load the Salon scene
+    public void LoadSalonScene()
+    {
+        LoadScene("Salon");
+    }
+
+    // Method to load the Salle de bain scene
+    public void LoadSalleDeBainScene()
+    {
+        LoadScene("Salle de bain");
     }
 }
